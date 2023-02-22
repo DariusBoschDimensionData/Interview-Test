cdb32d4018c8d42f518687338211320879b96997
Hexx123321 <thulane.alfred@gmail.com>
2023-02-22 12:22:10 +0200

    Add Hero Project Tests

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -15,26 +15,38 @@ namespace InterviewTest.Controllers
 
         public void Delete(int id)
         {
+            try
+            {
+                Console.WriteLine("Delete Works!");
+            }
+            catch {
+                Console.WriteLine("in the catch block");
+            }
             throw new NotImplementedException();
         }
 
         public void Evolve(int statIncrease = 5)
         {
+            Console.WriteLine("yippee evolve accessed! " + statIncrease);
             
         }
 
         public void Evolve(Hero hero)
         {
+            Console.WriteLine("Evolve but with a different signature, input param is Hero which is not good I guess lmao -> " + hero.name);
             throw new NotImplementedException();
         }
 
         public IEnumerable<Hero> Get()
         {
+            Console.WriteLine(name + "Is our current hero name in the overloaded Evolve with enumerable");
             throw new NotImplementedException();
         }
 
         public Hero Get(int id)
         {
+            Console.WriteLine($"{name} {id}" + "Yeah get you know...");
+
             throw new NotImplementedException();
         }
 
---
+++ InterviewTestTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InterviewTestTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterviewTestTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63eabfda-7b52-446e-ab1a-e28e6103cd56")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
