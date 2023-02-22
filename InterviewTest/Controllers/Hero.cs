ac2adf3195187b3500cc1df6b679404fe1522d87
Hexx123321 <thulane.alfred@gmail.com>
2023-02-22 11:32:26 +0200

    Added Hero Interface Implementation

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -1,18 +1,51 @@
-﻿using System;
+﻿using InterviewTest.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero : IHero
     {
         public string name { get; set; }
         public string power { get; set; }
         public List<KeyValuePair<string, int>> stats {get;set;}
-        public void evolve(int statIncrease = 5)
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Evolve(int statIncrease = 5)
         {
             
+        }
+
+        public void Evolve(Hero hero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hero> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hero Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ InterviewTest/Interfaces/IHero.cs
@@ -0,0 +1,21 @@
+﻿using InterviewTest.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace InterviewTest.Interfaces
+{
+    public interface IHero
+    {
+        IEnumerable<Hero> Get();
+
+        Hero Get(int id);
+
+        void Post([FromBody] string value);
+
+        void Put(int id, [FromBody] string value);
+
+        void Delete(int id);
+
+        void Evolve(Hero hero);
+    }
+}
