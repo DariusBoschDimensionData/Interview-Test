3af01446534098668ac82976ceeecc82830eb112
simontumelomatlou <simontumelomatlou@gmail.com>
2023-03-06 15:03:16 +0200

    DD Test

---
+++ InterviewTest/Controllers/IHero.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewTest.Controllers
+{
+    public interface IHero
+    {
+        string name { get; set; }
+        string power { get; set; }
+        List<KeyValuePair<string, int>> stats { get; set; }
+
+        void evolve(int statIncrease = 2);
+    }
+}
